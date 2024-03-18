6ece767b419ffdd791e31a69c893891fa7a572ec
kashifansaricodes <kansari@umd.edu>
2024-03-18 04:04:25 -0400

    running code updated. action set yet to be modified

--- A_star.py
+++ A_star.py
@@ -5,6 +5,9 @@ import heapq
 
 # Generating map
 map = np.zeros((500, 1200, 3))
+red = np.array([255, 0, 0])
+blue = np.array([0, 0, 255])
+black = np.array([0, 0, 0])
 
 # Generating rectangular clearance
 cv2.rectangle(map, pt1=(95, 0), pt2=(180, 405), color=(0, 0, 255), thickness=-1)
@@ -40,36 +43,31 @@ plt.title("Initial Map (close this window to continue)")
 plt.show()
 
 # Define the actions set
-actions_set = [(1.5*np.radian(np.cos(-60)), 1.5*np.radian(np.cos(-60))), (1.5*np.radian(np.cos(-30)), 1.5*np.radian(np.cos(-30))), 
-               (1.5*np.radian(np.cos(0)), 1.5*np.radian(np.cos(0))), (1.5*np.radian(np.cos(30)), 1.5*np.radian(np.cos(30))) 
-               (1.5*np.radian(np.cos(60)), 1.5*np.radian(np.cos(60)))]
-# cost_straight = 1.0
-# cost_diagonal = 1.4
-cost_0 = np.cos(0)
-cost_30 = np.cos(30)
-cost_60 = np.cos(60)
-
+actions_set = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+cost_straight = 1.0
+cost_diagonal = 1.4
 video_name = 'D:\Desktop\djikstra_scan.mp4'
 out = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), 10, (map.shape[1], map.shape[0]))
 
 # Define the function to calculate the cost of moving from one node to another
 def calculate_cost(current_cost, action):
-    if action in [(1.5*np.radian(np.cos(-60)), 1.5*np.radian(np.cos(-60))), (1.5*np.radian(np.cos(60)), 1.5*np.radian(np.cos(60)))]:
-        return current_cost + cost_60
-    if action in [(1.5*np.radian(np.cos(-30)), 1.5*np.radian(np.cos(-30))), (1.5*np.radian(np.cos(30)), 1.5*np.radian(np.cos(30)))]: 
-        return current_cost + cost_30
+    if action in [(1, 1), (-1, 1), (1, -1), (-1, -1)]:
+        return current_cost + cost_diagonal
     else:
-        return current_cost + cost_0
+        return current_cost + cost_straight
 
-# Dijkstra's algorithm function
-def dijkstra(start, goal, map):
+# Define the heuristic function (Euclidean distance)
+def heuristic(node, goal):
+    return np.sqrt((goal[0] - node[0])**2 + (goal[1] - node[1])**2)
+
+# A* algorithm function
+def a_star(start, goal, map):
     open_list = []
     closed_list = set()
-    visited_list = []
-    heapq.heappush(open_list, (0, start)) # 0 being the priority for start
-    came_from = {} # parent nodes dictionary
-    cost_so_far = {start: 0} # node: cost to reach
-    path_iteration =    0
+    heapq.heappush(open_list, (0, start))  # 0 being the priority for start
+    came_from = {}  # parent nodes dictionary
+    cost_so_far = {start: 0}  # node: cost to reach dictionary
+    path_iteration = 0
     while open_list:
         current_cost, current_node = heapq.heappop(open_list)
 
@@ -84,30 +82,31 @@ def dijkstra(start, goal, map):
             return path
 
         closed_list.add(current_node)
-        visited_list.append(current_node)
-        
+
         for action in actions_set:
             dx, dy = action
             next_node = (current_node[0] + dx, current_node[1] + dy)
 
-            if (5 <= next_node[0] < (map.shape[0]-5) and 5 <= next_node[1] < (map.shape[1]-5) and
-                    next_node not in closed_list and np.all(map[next_node[0], next_node[1]] == 0)):
+            if (5 <= next_node[0] < (map.shape[0]-5)) and (5 <= next_node[1] < (map.shape[1]-5)) and (next_node not in closed_list) and (np.array_equal(map[next_node[0], next_node[1]], black)):
                 new_cost = calculate_cost(cost_so_far[current_node], action)
+                heuristic_cost = heuristic(next_node, goal)  # Calculate heuristic cost
+                priority = new_cost + heuristic_cost  # Update priority with heuristic cost
 
                 if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
                     cost_so_far[next_node] = new_cost
-                    priority = new_cost
                     heapq.heappush(open_list, (priority, next_node))
                     came_from[next_node] = current_node
                     # Update the color of the scanned pixel to green
                     map[next_node[0], next_node[1]] = [0, 255, 0]  # Green color
-                    if path_iteration%10000==0:
+                    # frame1 = cv2.cvtColor(map.astype(np.uint8), cv2.COLOR_RGB2BGR)
+                    # out.write(frame1)
+                    if path_iteration % 10000 == 0:
                         frame = cv2.cvtColor(map.astype(np.uint8), cv2.COLOR_RGB2BGR)
                         out.write(frame)
-                    path_iteration+=1
-                    
+                    path_iteration += 1
 
     return None  # No path found
+
 
 # Prompt user for the start coordinates
 while True:
@@ -115,15 +114,19 @@ while True:
     start_y = int(input("Enter your start y-coordinate: "))
 
     # Access the pixel value using the index
-    start_pixel_value = map[start_y, start_x]
+    if (5 <= start_y <= (map.shape[0]-5)) and (5 <= start_x < (map.shape[1]-5)):
+        start_pixel_value = [int(value) for value in map[start_y, start_x]]
+    else:
+        print("Your values are not within boundary or on clearances. Please re-enter the start coordinates.")
+        continue
 
     # Check if the pixel value corresponds to red or blue and if the start coordinates are on the boundary
-    if (start_pixel_value == [255, 0, 0]).all() or (start_pixel_value == [0, 0, 255]).all() or (5 >= start_x >= (map.shape[0]-5)) or (5 >= start_y < (map.shape[1]-5)):
+    if np.array_equal(start_pixel_value, red) or np.array_equal(start_pixel_value, blue) or (5 >= start_x >= (map.shape[0]-5)) or (5 >= start_y < (map.shape[1]-5)):
         print("Your start is on an obstacle. Please re-enter the start coordinates.")
 
     else:
-        print("Your start cordinates are correct, proceed ahead" )
-        break   
+        print("Your start cordinates are correct, proceed ahead >>" )
+        break
 
 # Prompt user for the goal coordinates
 while True:
@@ -132,22 +135,26 @@ while True:
     goal_y = int(input("Enter your goal y-coordinate: "))
 
     # Access the pixel value using the index
-    goal_pixel_value = map[goal_y, goal_x]
+    if (5 <= goal_y <= (map.shape[0]-5)) and (5 <= goal_x <= (map.shape[1]-5)):
+        goal_pixel_value = [int(value) for value in map[goal_y, goal_x]]
+    else:
+        print("Your values are not within boundary or on clearances. Please re-enter the goal coordinates.")
+        continue
 
     # Check if the pixel value corresponds to red or blue and if the goal coordinates are on the boundary
-    if (goal_pixel_value == [255, 0, 0]).all() or (goal_pixel_value == [0, 0, 255]).all() or (5 >= goal_x >= (map.shape[0]-5)) or (5 >= goal_x < (map.shape[1]-5)):
+    if np.array_equal(goal_pixel_value, red) or np.array_equal(goal_pixel_value, blue):
+
         print("Your goal is on an obstacle. Please re-enter the goal coordinates.")
         print(goal_pixel_value)
     else:
         print("Your goal cordinates are correct. Search in progress PLEASE WAIT..." )
         print("Goal coordinate has following pixel values: ", goal_pixel_value)
-        cv2.circle(map, (goal_x, goal_y), 5, (255, 255, 255), -1)  # Draw a green circle at the goal
-        break 
+        break
 
-# Run Dijkstra's algorithm
-start_node = (start_x, start_y)  # Assuming the robot starts from the bottom-left corner
-goal_node = (goal_x, goal_y)
-path = dijkstra(start_node, goal_node, map)
+# Run A_star's algorithm
+start_node = (start_y, start_x)  # Assuming the robot starts from the bottom-left corner
+goal_node = (goal_y, goal_x)
+path = a_star(start_node, goal_node, map)
 
 path_iteration = 0
 # Print the path
@@ -160,7 +167,7 @@ if path :
             frame = cv2.cvtColor(map.astype(np.uint8), cv2.COLOR_RGB2BGR)
             out.write(frame)
         path_iteration+=1
-        
+
 else:
     print("No path found.")
 
